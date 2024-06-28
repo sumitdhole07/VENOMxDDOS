ff0cd8ea6a75bbe34b2e59b2c96eee97536ea96a
sumitdhole07 <sumitdhole07@gmail.com>
2024-06-28 12:08:41 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,5 @@
-from flask import Flask, render_template
-from threading import Thread
-
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return "Alive"
-
-def run():
-    app.run(host='0.0.0.0',port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()    
+import os
+print("Загрузка модулей не останавливайте код")
+os.system("pip install pyTelegramBotAPI")
+os.system("pip install DDos")
+os.system("chmod +x bgmi")
